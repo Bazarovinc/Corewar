364d2c07812c9ee5659adf5c24ebd3f06472e971
ddamaris <yzu@mail.ru>
2020-07-19 20:43:06 +0300

    modify free

--- src/free_vm.c
+++ src/free_vm.c
@@ -12,43 +12,45 @@
 
 #include "vm.h"
 
-static void free_players(t_player **players, int p_num)
+static void free_players(t_vm *vm)
 {
 	int i;
 
 	i = 0;
-	while (i < p_num)
+	while (i < MAX_PLAYERS)
 	{
-		if (players[i]->name)
-			free(players[i]->name);
-		if (players[i]->comment)
-			free(players[i]->comment);
-		if (players[i]->code)
-			free(players[i]->code);
-		if (players[i])
-			free(players[i]);
+		if (vm->players[i])
+		{
+			if (vm->players[i]->name)
+				free(vm->players[i]->name);
+			if (vm->players[i]->comment)
+				free(vm->players[i]->comment);
+			if (vm->players[i]->code)
+				free(vm->players[i]->code);
+			free(vm->players[i]);
+		}
 		i++;
 	}
 }
 
-static void free_cursor(t_cursor **cursor)
+static void free_cursor(t_vm *vm)
 {
-	t_cursor	*current;
+	t_cursor	*cursor;
 	t_cursor	*delete;
 
-	current = *cursor;
-	while (current)
+	cursor = vm->cursors;
+	while (cursor)
 	{
-		delete = current;
-		current = current->next;
+		delete = cursor;
+		cursor = cursor->next;
 		ft_memdel((void **)&delete);
 	}
-	*cursor = NULL;
+	vm->cursors = NULL;
 }
 
 void		free_vm(t_vm *vm)
 {
-	free_players(*(vm->players), vm->players_num);
-	free_cursor(&(vm->cursors));
+	free_players(vm);
+	free_cursor(vm);
 	free(vm);
 }
